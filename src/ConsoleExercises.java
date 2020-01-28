6f3ab2abfb493f36f78e6d013a2f61da6d9fc1b8
Gaston Tarango <gaston.tarango@gmail.com>
2020-01-28 11:47:11 -0600

    started control flow

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.format("The value of " + pi + " is approximately 3.14.%n");
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int i = 0;
+//        while (i != -1){
+//            System.out.print("Enter interger: ");
+//            int userInput = scanner.nextInt();
+//            System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        }
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter something: ");
+//        String userInput = scanner.next();
+//        String userInput2 = scanner.next();
+//        String userInput3 = scanner.next();
+//
+//        System.out.println("You entered: --> \"" + userInput +" " + userInput2 + " " + userInput3 + "\" <--");
+//        scanner.nextLine();
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter something: ");
+//        String userInput = scanner.nextLine();
+//        String userInput2 = scanner.nextLine();
+//        String userInput3 = scanner.nextLine();
+//
+//        System.out.println("You entered: --> \"" + userInput +" " + userInput2 + " " + userInput3 + "\" <--");
+//        scanner.nextLine();
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        int userInput = scanner.nextInt();
+        int userInput1 = scanner.nextInt();
+        int x = userInput + userInput + userInput1 + userInput1;
+        int y = userInput * userInput1;
+
+        System.out.println("The perimeter for the input entered: --> \"" + x + "\" <--");
+        System.out.println("The Area for the input entered: --> \"" + y + "\" <--");
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        scanner,useDelimiter("\n");
+//        system.out.print("\nEnter the width");
+//        String sWidth =
+
+
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,54 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello,");
-        System.out.print("World!");
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 19;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is a String";
+        myString = "3.14159";
+        System.out.println(myString);
+
+        float myNumber;
+        myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        myNumber += 9;
+//         equivalent to
+//        myNumber = myNumber + 9;
+
+//        int x = 4;
+//        x = x + 5;
+
+//        x += 5;
+//
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        y *= x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+//        x /= y;
+//        y -= x;
+
     }
 }

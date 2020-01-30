fe653a0b089cf66ece5827ace7adaa58b49b242b
Gaston Tarango <gaston.tarango@gmail.com>
2020-01-30 16:51:26 -0600

    started game

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -11,6 +11,7 @@ public class MethodsExercises {
 //        division(3, 2);
 //        mod(10, 2);
 //        getInteger(1,10);
+        AdventureGame();
         boolean again;
 
 //        do {
@@ -20,16 +21,16 @@ public class MethodsExercises {
 //            again = cont.equalsIgnoreCase("yes");
 //        } while (again);
 
-        do {
-            System.out.println("How many sides on the dice?");
-            rollDice(getInteger(1,100));
+//        do {
+//            System.out.println("How many sides on the dice?");
+//            rollDice(getInteger(1,100));
+//
+//
+//            System.out.println("Would you like to continue? Enter yes or no");
+//        }while(sc.nextLine().equalsIgnoreCase("yes"));
+//
 
-
-            System.out.println("Would you like to continue? Enter yes or no");
-        }while(sc.nextLine().equalsIgnoreCase("yes"));
-
-
-        }
+    }
 
     public static void addition(int first, int second) {
         System.out.println(first + second);
@@ -61,46 +62,145 @@ public class MethodsExercises {
         return numInput;
     }
 
-    public static void getFactorial() {
-        int userNumber = getInteger(1, 10);
-        long factorial = 1;
-        for (int i = 1; i <= userNumber; i++) {
-            factorial *= i;
-            if (i == userNumber) {
-                System.out.println(userNumber + "! = " + factorial);
-            }
-        }
-    }
+//    public static void getFactorial() {
+//        int userNumber = getInteger(1, 10);
+//        long factorial = 1;
+//        for (int i = 1; i <= userNumber; i++) {
+//            factorial *= i;
+//            if (i == userNumber) {
+//                System.out.println(userNumber + "! = " + factorial);
+//            }
+//        }
+//    }
 
-    public static void rollDice(int sides) {
-        Random random = new Random();
-        int dice1 = random.nextInt(sides) + 1;
-        int dice2 = random.nextInt(sides) + 1;
-        int sum = dice1 + dice2;
-
-        System.out.println("dice1 = " + dice1);
-        System.out.println("dice2 = " + dice2);
-        System.out.println(sum);
-    }
+//    public static void rollDice(int sides) {
+//        Random random = new Random();
+//        int dice1 = random.nextInt(sides) + 1;
+//        int dice2 = random.nextInt(sides) + 1;
+//        int sum = dice1 + dice2;
+//
+//        System.out.println("dice1 = " + dice1);
+//        System.out.println("dice2 = " + dice2);
+//        System.out.println(sum);
+//    }
 
     public static void AdventureGame() {
-        int warriorhealth = (int) (Math.random() * 20) + 50;
-        int warriorAttack = (int) (Math.random() * 10) + 15;
-        int warriorDefence = (int) (Math.random() * 10) + 50;
-        int magehealth = (int) (Math.random() * 20) + 50;
-        int mageAttack = (int) (Math.random() * 10) + 20;
-        int mageDefence = (int) (Math.random() * 10) + 40;
-        int miniLhealth = (int) (Math.random() * 10) + 30;
-        int miniLAttack = (int) (Math.random() * 10) + 5;
-        int miniLDefence = (int) (Math.random() * 5) + 40;
-        int miniRhealth = (int) (Math.random() * 10) + 30;
-        int miniRAttack = (int) (Math.random() * 10) + 5;
-        int miniRDefence = (int) (Math.random() * 5) + 40;
-        int mainBhealth = (int) (Math.random() * 20) + 50;
-        int mainBAttack = (int) (Math.random() * 10) + 10;
-        int mainBDefence = (int) (Math.random() * 15) + 40;
+        int mageHealth = (int) (Math.random() * 20) + 70;
+        int mageAttack = (int) (Math.random() * 10) + 10;
+        int mageDefense = (int) (Math.random() * 5) + 5;
+        int fireball = (int) (Math.random() * 15) + 25;
+        int mageMana = (int) (Math.random() * 30) + 20;
+//        int warriorHealth = (int) (Math.random() * 20) + 80;
+//        int warriorAttack = (int) (Math.random() * 10) + 20;
+//        int warriorDefense = (int) (Math.random() * 20) + 60;
+        int goblinHealth = (int) (Math.random() * 20) + 50;
+        int goblinAttack = (int) (Math.random() * 5) + 10;
+        int goblinEnrageAttack = (int) (Math.random() * 15) + 15;
 
-        ChooseHero =
 
-    }
-}
+        int fighterClassNum = 0;
+        String fighterClass;
+
+        int health;
+//        int mana;
+        int attack;
+        int defense;
+        int currentHealth = goblinHealth;
+
+        System.out.println("Would you like to go on an adventure? yes or no");
+        Scanner sc = new Scanner(System.in);
+
+        String userInput = sc.nextLine().toLowerCase();
+        if (userInput.equals("yes")) {
+            System.out.println("Let's begin");
+            System.out.println("What is your name, traveler?");
+            String userName = sc.nextLine().toLowerCase();
+            System.out.println("Welcome " + userName);
+            System.out.println("Choose your class: Press 1 for mage!");
+            fighterClassNum = sc.nextInt();
+        }
+        if (fighterClassNum == 1) {
+            fighterClass = "Mage";
+            health = mageHealth;
+//            mana = mageMana;
+            attack = fireball;
+            defense = mageDefense;
+            System.out.println("You have chosen " + fighterClass + '.');
+            System.out.println("Your stats are\nHealth: " + health + "\n" + "Attack: " + attack + "\n" + "Defense: " + defense);
+            System.out.println("Let us begin. You are traveling through a dungeon and you are approached by an ugly looking goblin. It snarls and draws it's weapon.");
+            System.out.println("Would you like to fight the goblin or attempt to reason? (fight or reason)");
+            sc.nextLine();
+            String answer = sc.nextLine();
+            if (answer.trim().equalsIgnoreCase("reason")) {
+                System.out.println("You dont speak goblin how can you reason with it you idiot");
+                health -= goblinAttack;
+                System.out.println("The goblin is angered at your disgusting human voice and swings its club, dealing " + goblinAttack + " damage. Your health is now " + (health + "!"));
+                answer = "fight";
+            }
+            while (answer.equalsIgnoreCase("fight")) {
+
+                System.out.println("What now? Attack with a fireball");
+                String fightOption = sc.nextLine();
+                if (fightOption.equalsIgnoreCase("attack")) {
+                    currentHealth -= mageAttack;
+                    System.out.println("You cast a fireball dealing " + attack + " damage! The goblin now has " + currentHealth + " health left.");
+                    if (currentHealth > 0) {
+                        health -= goblinAttack;
+                        System.out.println("The fight isn't over yet. The goblin strikes you with its club dealing " + goblinAttack + " damage. Your remaining health is " + health + "!");
+                        if (currentHealth <= goblinHealth / 2) {
+                            goblinAttack = goblinEnrageAttack;
+                            System.out.println("The goblin is furious at your attacks and goes into a rage, doubling his damage!");
+                        }
+//                        if (fightOption.equalsIgnoreCase("fireball")) {
+//                            if (mana >= 15) {
+//                                currentHealth -= fireball;
+//                                mana -= 10;
+//                                System.out.println("You cast your fireball spell, dealing " + fireball + " damage! The goblin now has " + currentHealth + " health left. Your remaining mana is " + mana + ".");
+//                            }
+//                            if (mana < 15) {
+//                                System.out.println("You need more mana");
+////
+//                            }
+//                                if (currentHealth > 0) {
+//                                    health -= goblinAttack;
+//                                    System.out.println("The fight isn't over yet. The goblin strikes you with its club dealing " + goblinAttack + " damage. Your remaining health is " + health + "!");
+//                                    if (currentHealth <= goblinHealth / 2) {
+//                                        goblinAttack = goblinEnrageAttack;
+//                                        System.out.println("The goblin is furious at your attacks and goes into a rage, doubling his damage!");
+//                                    }
+//                                }
+                            }
+                            if (goblinHealth == 0) {
+                                System.out.println("The goblin is dead!");
+                            }
+                            if (health == 0) {
+                                System.out.println("You died.");
+                            }
+
+                        }
+//                } else if (fighterClassNum == 2) {
+//                    fighterClass = "Warrior";
+//                    health = warriorHealth;
+//                    mana = 0;
+//                    attack = warriorAttack;
+//                    defense = warriorDefense;
+//                    System.out.println("You have chosen " + fighterClass + '.');
+//                    System.out.println("Your stats are\nHealth: " + health + "\n" + "Mana: " + mana + "\n" + "Attack: " + attack + "\n" + "Defense: " + defense);
+//                    System.out.println("Let us begin. You are traveling along a cobblestone road and you are approached by an ugly looking goblin. It snarls at you menacingly and draws it's weapon.");
+//                    System.out.println("Would you like to fight the goblin or attempt to reason?");
+//                    sc.nextLine();
+//                    String answer = sc.nextLine();
+//                    System.out.println(health);
+//                    if (answer.equalsIgnoreCase("reason")) {
+//                        System.out.println("There is no reasoning with a beast like this");
+//                        System.out.println("The goblin is angered at your disgusting human voice and lunges at you with its club, dealing " + goblinAttack + " damage. Your health is now " + (health - goblinAttack + "!"));
+//                        sc.nextLine();
+//                        answer = "fight";
+                    }
+                }
+            }
+
+        }
+
+
+

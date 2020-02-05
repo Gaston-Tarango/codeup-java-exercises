8ad626b6f45cd6e696f3ea537a5c119e3a9f7310
Gaston Tarango <gaston.tarango@gmail.com>
2020-02-05 10:37:08 -0600

    lect

---
+++ src/Vehicle/Vehicle.java
@@ -0,0 +1,11 @@
+package Vehicle;
+
+import org.w3c.dom.ls.LSOutput;
+
+public interface Vehicle {
+     void go(double miles);
+     void method1();
+     void method2(int num);
+     int method3(String message);
+
+}
---
+++ src/Vehicle/VehicleApp.java
@@ -0,0 +1,8 @@
+package Vehicle;
+
+public class VehicleApp {
+    public static void main(String[] args){
+        Vehicle vehicle = new Car();
+        vehicle.go(3);
+    }
+}

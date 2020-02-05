85f00677f7dc564e96abe65a5912449f2af8e8c5
Gaston Tarango <gaston.tarango@gmail.com>
2020-02-05 16:22:12 -0600

    .

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,11 +4,12 @@ public class ShapesTest {
     public static void main(String[] args) {
         Measureable myShape;
         myShape = new Square(5);
-        System.out.println("myShape.getArea() = " + myShape.getArea());
-        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
         myShape = new Rectangle(5, 4);
-        System.out.println("myShape.getArea() = " + myShape.getArea());
-        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+
 
     }
 }
